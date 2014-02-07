e461c54ccc29f03674fcec5b0efbb8581218a67d
David Császár <dcsaszar@users.noreply.github.com>
2014-02-07 15:11:17 +0100

    Add find method to client

--- lib/ntswf/client.rb
+++ lib/ntswf/client.rb
@@ -22,11 +22,11 @@ module Ntswf
     #   The executing unit's key, a corresponding activity task list must be configured
     # @option options [Numeric] :version
     #   Optional minimum version of the client. The task may be rescheduled by older clients.
-    # @return [AWS::SimpleWorkflow::WorkflowExecution]
+    # @return (see #find)
     # @raise [AWS::SimpleWorkflow::Errors::WorkflowExecutionAlreadyStartedFault]
     def start_execution(options)
       execution_id = options.delete(:execution_id)
-      workflow_type.start_execution(
+      workflow_execution = workflow_type.start_execution(
         child_policy: :terminate,
         execution_start_to_close_timeout: 48 * 3600,
         input: options.to_json,
@@ -35,9 +35,123 @@ module Ntswf
         task_start_to_close_timeout: 10 * 60,
         workflow_id: [activity_task_list, execution_id].join(separator),
       )
+      execution_details(workflow_execution).merge!(
+        name: options[:name].to_s,
+        params: options[:params],
+      )
+    end
+
+    # Get status and details of a workflow execution.
+    # @param ids [Hash] Identifies the queried execution
+    # @option ids [String] :workflow_id Workflow ID
+    # @option ids [String] :run_id Run ID
+    # @raise [AWS::SimpleWorkflow::Errors::UnknownResourceFault]
+    # @return [Hash]
+    #   Execution properties.
+    #   :exception:: Exception message for an unexpectedly failed execution
+    #   :error:: Error message returned from an execution
+    #   :outcome:: Result of a completed execution
+    #   :params:: Custom params from JSON
+    #   :run_id:: The workflow execution's run ID
+    #   :status:: Calculated workflow execution status (:completed, :open, others indicating failure)
+    #   :name:: Given task kind
+    #   :workflow_id:: The workflow execution's workflow ID
+    def find(ids)
+      workflow_execution = domain.workflow_executions.at(ids[:workflow_id], ids[:run_id])
+      history_details(workflow_execution)
     end
 
     protected
+
+    def execution_details(workflow_execution)
+      {
+        workflow_id: workflow_execution.workflow_id,
+        run_id: workflow_execution.run_id,
+        status: workflow_execution.status,
+      }
+    end
+
+    def history_details(workflow_execution)
+      result = execution_details(workflow_execution)
+      input = parse_input workflow_execution.history_events.first.attributes.input
+      result.merge!(name: input["name"].to_s, params: input["params"])
+
+      case result[:status]
+      when :open
+        # nothing
+      when :completed
+        result.merge!(completion_details workflow_execution)
+      else
+        result.merge!(failure_details workflow_execution)
+      end
+      result
+    end
+
+    def completion_details(workflow_execution)
+      completed_event = workflow_execution.history_events.reverse_order.detect do |e|
+        e.event_type == "WorkflowExecutionCompleted"
+      end
+      if completed_event
+        {outcome: parse_attribute(completed_event, :result)["outcome"]}
+      else
+        {status: :open}
+      end
+    end
+
+    TERMINAL_EVENT_TYPES_ON_FAILURE = %w(
+      WorkflowExecutionFailed
+      WorkflowExecutionTimedOut
+      WorkflowExecutionCanceled
+      WorkflowExecutionTerminated
+    )
+
+    def failure_details(workflow_execution)
+      terminal_event = workflow_execution.history_events.reverse_order.detect {|e|
+        TERMINAL_EVENT_TYPES_ON_FAILURE.include?(e.event_type)
+      }
+      if terminal_event
+        event_type = terminal_event.event_type
+        case event_type
+        when "WorkflowExecutionFailed"
+          details = parse_attribute(terminal_event, :details)
+          {
+            error: details["error"],
+            exception: details["exception"],
+          }
+        else
+          {
+            error: event_type,
+            exception: event_type,
+          }
+        end
+      else
+        log("No terminal event for execution"\
+            " #{workflow_execution.workflow_id} | #{workflow_execution.run_id}."\
+            " Event types: #{workflow_execution.history_events.map(&:event_type)}") rescue nil
+        {
+          error: "Execution has finished with status #{workflow_execution.status},"\
+              " but did not provide details."
+        }
+      end
+    end
+
+    def parse_attribute(event, key)
+      value = nil
+      begin
+        json_value = event.attributes[key]
+      rescue ArgumentError
+        # missing key in event attributes
+      end
+      if json_value
+        begin
+          value = JSON.parse json_value
+        rescue # JSON::ParserError, ...
+          # no JSON
+        end
+      end
+      value = nil unless value.kind_of? Hash
+      value || {}
+    end
 
     def workflow_type
       @workflow_type ||= domain.workflow_types[workflow_name, workflow_version]
