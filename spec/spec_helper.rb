6aa9887222425836d648079dbf03da01d812b35a
Tilo Prütz <tilo@infopark.de>
2015-06-18 16:59:15 +0200

    upgraded rspec to 3.3

---
+++ spec/spec_helper.rb
@@ -0,0 +1,15 @@
+require 'rspec/its'
+
+RSpec.configure do |config|
+  config.order = :random
+  Kernel.srand config.seed
+
+  config.expect_with :rspec do |expectations|
+    expectations.syntax = :expect
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.syntax = :expect
+    mocks.verify_partial_doubles = true
+  end
+end
